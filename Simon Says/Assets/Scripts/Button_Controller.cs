d19362f11ed71499da76972a268069d70aebb5d8
Simar2906 <simar2906@gmail.com>
2020-08-13 15:24:31 +0530

    click action added(with minor bug)

---
+++ Simon Says/Assets/Scripts/Button_Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Controller : MonoBehaviour
+{
+    private SpriteRenderer theSprite;
+
+    void Start()
+    {
+        theSprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        theSprite.color = new Color(theSprite.color.r, theSprite.color.g, theSprite.color.b, 1f);
+    }
+
+    private void OnMouseUp()
+    {
+        theSprite.color = new Color(theSprite.color.r, theSprite.color.g, theSprite.color.b, 0.5f);
+
+    }
+}
