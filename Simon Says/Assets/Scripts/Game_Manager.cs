05b5860d43ef347e6d3df8adc2b504e956286ee5
Simar2906 <simar2906@gmail.com>
2020-08-13 14:53:19 +0530

    button light up setup done

---
+++ Simon Says/Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour
+{
+    public SpriteRenderer[] colours;
+    private int colourSelect = 0;
+    public float stayLit = 0f;
+    private float stayLitCounter = 0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stayLitCounter > 0)
+        {
+            stayLitCounter -= Time.deltaTime;
+        }
+        else
+        {
+            colours[colourSelect].color =  new Color (colours[colourSelect].color.r, colours[colourSelect].color.g, colours[colourSelect].color.b, 0.5f);
+        }
+    }
+    public void StartGame()
+    {
+        colourSelect = Random.Range(0, colours.Length);
+        colours[colourSelect].color =  new Color (colours[colourSelect].color.r, colours[colourSelect].color.g, colours[colourSelect].color.b, 1f);
+        stayLitCounter = stayLit;
+    }
+
+}
